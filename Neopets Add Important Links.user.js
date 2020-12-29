993138b8b3b8daea3336992d245ab6ddbf05c764
Harvey <gumdropghost@gmail.com>
2020-12-29 18:28:05 -0500

    Create Neopets Add Important Links.user.js

---
+++ Neopets Add Important Links.user.js
@@ -0,0 +1,39 @@
+// ==UserScript==
+// @name         Neopets Add Important Links
+// @version      0.1
+// @description  Adds some missing links to the sidebar
+// @author       Harvey
+// @match        http://www.neopets.com/*
+// @grant        none
+// ==/UserScript==
+
+var $ = window.jQuery;
+
+//returns true if white
+//returns false if black
+function getLinkHtml(link,icon,text)
+{
+    var linkadd ="<a href=\"" +link + "\"><li class=\"nav-link-leave-beta__2020\" data-url=\"/" + link + "\"><div class=\"" + icon +"\"></div>" + text +"</li></a>";
+    return linkadd;
+}
+
+function addLinks()
+{
+    var addedLinks = "";
+    var quickref = getLinkHtml("quickref.phtml", "nav-petcentral-icon__2020", "Quickref");
+    var quickstock = getLinkHtml("quickstock.phtml", "nav-userlookup-icon__2020", "Quickstock");
+    var sdb = getLinkHtml("safetydeposit.phtml", "nav-inventory-icon__2020", "Safety Deposit Box");
+    addedLinks = addedLinks + quickref;
+    addedLinks = addedLinks + quickstock;
+    addedLinks = addedLinks + sdb;
+
+    var clock = document.getElementsByClassName("nav-profile-dropdown-clock__2020")[0];
+
+    var firstSet = document.getElementById("navprofiledropdown__2020");
+    var newEl = document.createElement('ul');
+    newEl.innerHTML = addedLinks;
+    clock.parentNode.insertBefore(newEl, clock.nextSibling.nextSibling);
+
+}
+
+addLinks();
