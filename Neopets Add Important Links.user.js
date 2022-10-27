fc887794201485e9ccd58c539631238e28572c99
Harvey <6914258+Blathers@users.noreply.github.com>
2022-10-27 00:12:38 -0400

    Added right margin to inventory link

--- Neopets Add Important Links.user.js
+++ Neopets Add Important Links.user.js
@@ -25,12 +25,12 @@ function addLinks()
     var quickstock = getLinkHtml("https://www.neopets.com/quickstock.phtml", "nav-userlookup-icon__2020", "Quickstock");
     var custom = getLinkHtml("https://www.neopets.com/customise/", "nav-userlookup-icon__2020", "Customization");
     var sdb = getLinkHtml("https://www.neopets.com/safetydeposit.phtml", "nav-inventory-icon__2020", "Safety Deposit Box");
-    addedLinks = addedLinks + quickref;
-    addedLinks = addedLinks + quickstock;
-    addedLinks = addedLinks + custom;
-    addedLinks = addedLinks + sdb;
+    addedLinks = addedLinks + quickref; //Quickref link
+    addedLinks = addedLinks + quickstock; //Quickstock link
+    addedLinks = addedLinks + custom; //Customization link
+    addedLinks = addedLinks + sdb; //Safety deposit box link
 
-    var addInvLink = "<a href=\"/inventory.phtml\"><div class=\"navsub-np-meter__2020\" style=\"display: inline-block; margin-bottom: 0px;text-align:center;\"><div class=\"navsub-np-icon__2020\" style=\"background:none!important;\"><img src=\"https://images.neopets.com/themes/h5/hauntedwoods/images/inventory-icon.svg\" width=\"25\"></div><span id=\"npanchor\" class=\"np-text__2020\">Inventory</span></div></a>"
+    var addInvLink = "<a href=\"/inventory.phtml\"><div class=\"navsub-np-meter__2020\" style=\"display: inline-block;margin-right:5px; margin-bottom: 0px;text-align:center;\"><div class=\"navsub-np-icon__2020\" style=\"background:none!important;\"><img src=\"https://images.neopets.com/themes/h5/hauntedwoods/images/inventory-icon.svg\" width=\"25\"></div><span id=\"npanchor\" class=\"np-text__2020\">Inventory</span></div></a>"
 
     var clock = document.getElementsByClassName("nav-profile-dropdown-clock__2020")[0];
 
