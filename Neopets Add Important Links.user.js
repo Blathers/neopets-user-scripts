3cb054ce038466ce112ae615cc5ed92b4790ff32
Harvey <6914258+Blathers@users.noreply.github.com>
2022-10-26 23:50:36 -0400

    Add inventory link next to bank icon

--- Neopets Add Important Links.user.js
+++ Neopets Add Important Links.user.js
@@ -21,12 +21,16 @@ function getLinkHtml(link,icon,text)
 function addLinks()
 {
     var addedLinks = "";
-    var quickref = getLinkHtml("http://www.neopets.com/quickref.phtml", "nav-petcentral-icon__2020", "Quickref");
-    var quickstock = getLinkHtml("http://www.neopets.com/quickstock.phtml", "nav-userlookup-icon__2020", "Quickstock");
-    var sdb = getLinkHtml("http://www.neopets.com/safetydeposit.phtml", "nav-inventory-icon__2020", "Safety Deposit Box");
+    var quickref = getLinkHtml("https://www.neopets.com/quickref.phtml", "nav-petcentral-icon__2020", "Quickref");
+    var quickstock = getLinkHtml("https://www.neopets.com/quickstock.phtml", "nav-userlookup-icon__2020", "Quickstock");
+    var custom = getLinkHtml("https://www.neopets.com/customise/", "nav-userlookup-icon__2020", "Customization");
+    var sdb = getLinkHtml("https://www.neopets.com/safetydeposit.phtml", "nav-inventory-icon__2020", "Safety Deposit Box");
     addedLinks = addedLinks + quickref;
     addedLinks = addedLinks + quickstock;
+    addedLinks = addedLinks + custom;
     addedLinks = addedLinks + sdb;
+
+    var addInvLink = "<a href=\"/inventory.phtml\"><div class=\"navsub-np-meter__2020\" style=\"display: inline-block; margin-bottom: 0px;text-align:center;\"><div class=\"navsub-np-icon__2020\" style=\"background:none!important;\"><img src=\"https://images.neopets.com/themes/h5/hauntedwoods/images/inventory-icon.svg\" width=\"25\"></div><span id=\"npanchor\" class=\"np-text__2020\">Inventory</span></div></a>"
 
     var clock = document.getElementsByClassName("nav-profile-dropdown-clock__2020")[0];
 
@@ -36,6 +40,14 @@ function addLinks()
         var newEl = document.createElement('ul');
         newEl.innerHTML = addedLinks;
         clock.parentNode.insertBefore(newEl, clock.nextSibling.nextSibling);
+
+        var bankLink = document.getElementsByClassName("navsub-right__2020")[0];
+
+
+        var invEl = document.createElement('span');
+        invEl.innerHTML = addInvLink;
+
+        bankLink.insertBefore(invEl, bankLink.children[0]);
     }
 }
 
